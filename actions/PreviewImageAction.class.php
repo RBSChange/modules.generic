471d3a4b064237e816226585e9a5ade449d85d3a
Eric HAUSWALD <eric.hauswald@rbs.fr>
2010-06-09 10:18:47 +0000

    [NEW] Ajout du module theme

---
+++ actions/PreviewImageAction.class.php
@@ -0,0 +1,19 @@
+<?php
+class generic_PreviewImageAction extends f_action_BaseAction
+{
+	
+	/**
+	 * @param Context $context
+	 * @param Request $request
+	 */
+	protected function _execute($context, $request)
+	{
+		f_web_http_Header::setStatus(404);
+		return View::NONE;
+	}	
+	
+	public function isSecure()
+	{
+		return false;
+	}
+}

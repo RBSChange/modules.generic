c1a74e64c901fa8755409ec262dd8c5fe54872f2
Eric HAUSWALD <eric.hauswald@rbs.fr>
2011-04-01 13:01:52 +0200

    [UPDATE] Update loading common properties for document editor

---
+++ actions/GetDocumentEditorInfosAction.class.php
@@ -0,0 +1,16 @@
+<?php
+class generic_GetDocumentEditorInfosAction extends f_action_BaseJSONAction
+{
+	/**
+	 * @param Context $context
+	 * @param Request $request
+	 */
+	public function _execute($context, $request)
+	{
+		$document = $this->getDocumentInstanceFromRequest($request);
+		//Retrouve le document original
+		$document = DocumentHelper::getByCorrection($document);
+		$data = $document->getDocumentService()->getDocumentEditorInfos($document, $this->getModuleName());
+		return $this->sendJSON($data);
+	}
+}

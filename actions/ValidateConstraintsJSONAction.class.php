33ff0af5ae6d052084ff8cad12b93c2b2c06711a
Eric HAUSWALD <eric.hauswald@rbs.fr>
2011-06-10 12:20:25 +0200

    [FIX #35425] Langue de l'interface backoffice, message d'erreur d'une contrainte

--- actions/ValidateConstraintsJSONAction.class.php
+++ actions/ValidateConstraintsJSONAction.class.php
@@ -7,33 +7,46 @@ class generic_ValidateConstraintsJSONAction extends f_action_BaseJSONAction
 	 */
 	public function _execute($context, $request)
 	{
-		$data = $request->getParameter('data', '');
-		$constraintArray = $request->getParameter('constraints', array());
-		if (f_util_ArrayUtils::isNotEmpty($constraintArray))
+		$rc = RequestContext::getInstance();
+		try 
 		{
-			$constraints = implode(';', $constraintArray);
-			$name = $request->getParameter('name', '');
-			$property   = new validation_Property($name, $data);
-			$errors     = new validation_Errors();
-			$constraintsParser = new validation_ContraintsParser();
-			$validators = $constraintsParser->getValidatorsFromDefinition($constraints);
-	
-			foreach ($validators as $validator)
+			$rc->beginI18nWork($rc->getUILang());
+			$data = $request->getParameter('data', '');
+		
+			$constraintArray = $request->getParameter('constraints', array());
+			if (f_util_ArrayUtils::isNotEmpty($constraintArray))
 			{
-				try
+				$constraints = implode(';', $constraintArray);
+				$name = $request->getParameter('name', '');
+				$property   = new validation_Property($name, $data);
+				$errors     = new validation_Errors();
+				$constraintsParser = new validation_ContraintsParser();
+				$validators = $constraintsParser->getValidatorsFromDefinition($constraints);
+		
+				foreach ($validators as $validator)
 				{
-					$validator->validate($property, $errors);
+					try
+					{
+						$validator->validate($property, $errors);
+					}
+					catch (IllegalArgumentException $e)
+					{
+						return $this->sendJSONException($e, false);
+					}
+					if ($errors->count() > 0)
+					{
+						return $this->sendJSONError($errors[0], false);
+					}
 				}
-				catch (IllegalArgumentException $e)
-				{
-					return $this->sendJSONException($e, false);
-				}
-				if ($errors->count() > 0)
-				{
-					return $this->sendJSONError($errors[0], false);
-				}
-			}
+			}			
+			
+			$rc->endI18nWork();
+		} 
+		catch (Exception $e) 
+		{
+			$rc->endI18nWork($e);
 		}
+
 		return $this->sendJSON(array());
 	}
 }

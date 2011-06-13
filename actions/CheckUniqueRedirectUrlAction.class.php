fbb85fd58f8672e7bb9c520a73ba91dabc3eff0b
Alexandre SIMON <alexandre.simon@rbs.fr>
2011-06-13 18:10:33 +0200

    [FIX #35684] Url rewriting avec une libellé de document identique dans plusieurs langues

--- actions/CheckUniqueRedirectUrlAction.class.php
+++ actions/CheckUniqueRedirectUrlAction.class.php
@@ -11,8 +11,9 @@ class generic_CheckUniqueRedirectUrlAction extends f_action_BaseJSONAction
 		$websiteId = $document->getDocumentService()->getWebsiteId($document);
 		if ($websiteId === null) {$websiteId = 0;}
 		$checkUrl = $request->getParameter('from_url');
+		$lang = $request->getParameter('lang');
 		//rule_id, document_id, document_lang, website_id, to_url, redirect_type
-		$urlInfo = $this->getPersistentProvider()->getPageForUrl($checkUrl, $websiteId);
+		$urlInfo = $this->getPersistentProvider()->getPageForUrl($checkUrl, $websiteId, $lang);
 		if ($urlInfo !== null)
 		{
 			return $this->sendJSON($urlInfo);

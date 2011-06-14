571afbae04f57f6feba9f2ce4302651a51dd7f3e
Alexandre SIMON <alexandre.simon@rbs.fr>
2011-06-14 15:44:15 +0200

    [FIX #28681] Champs de l'onglet vides

--- actions/LoadJSONAction.class.php
+++ actions/LoadJSONAction.class.php
@@ -8,6 +8,13 @@ class generic_LoadJSONAction extends f_action_BaseJSONAction
 	public function _execute($context, $request)
 	{
 		$document = $this->getDocumentInstanceFromRequest($request);
+		// The following if is only valid for 3.0.4, Cf. http://www.rbschange.fr/tickets-35142.html
+		// In 3.5.0, this is handled by "http://git.rd.devlinux.france.rbs.fr/?p=modules.generic.git;a=commit;h=c1a74e64c901fa8755409ec262dd8c5fe54872f2"
+		if (!$request->hasParameter("lang"))
+		{
+			RequestContext::getInstance()->setLang($document->getLang());
+		}
+		
 		//Retrouve la correction courante
 		$document = DocumentHelper::getCorrection($document);
 		
@@ -20,6 +27,8 @@ class generic_LoadJSONAction extends f_action_BaseJSONAction
 				$allowedProperties[] = $propertyName;
 			}
 		}
+		
+		
 		
 		$data = $this->exportFieldsData($document, $allowedProperties);
 		

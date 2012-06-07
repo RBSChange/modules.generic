46adddbeb666ad2374221339d59de49894e5cde4
Gaël PORT <gael.port@rbs.fr>
2012-06-07 15:19:59 +0200

    [REFACTOR] Migrate framework classes.

--- actions/I18nSynchronizeJSONAction.class.php
+++ actions/I18nSynchronizeJSONAction.class.php
@@ -6,8 +6,8 @@
 class generic_I18nSynchronizeJSONAction extends change_JSONAction
 {
 	/**
-	 * @param Context $context
-	 * @param Request $request
+	 * @param change_Context $context
+	 * @param change_Request $request
 	 */
 	public function _execute($context, $request)
 	{

267b9cde422a25ef4328aed68cb98e8db458850e
Gaël PORT <gael.port@rbs.fr>
2012-09-19 17:41:57 +0200

    [DELETE] Delete old samples.

--- setup/initData.php
+++ setup/initData.php
@@ -3,14 +3,6 @@ class generic_Setup extends object_InitDataSetup
 {
 	public function install()
 	{
-		try
-		{
-			$this->executeModuleScript('lists.xml');
-		}
-		catch (Exception $e)
-		{
-			echo "ERROR: " . $e->getMessage() . "\n";
-			Framework::exception($e);
-		}
+		$this->executeModuleScript('init.xml');
 	}
 }
